3aaeecb20c582e5968197c6c065bf970f3b70663
woncodes <64626054+woncodes@users.noreply.github.com>
2024-04-30 23:03:36 -0400

    add main-container & grid-container

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const body = document.querySelector('body');
+const container = document.querySelector('#grid-container');
+
 const title = document.querySelector('#title-text');
 title.addEventListener('click', () => {
   window.location.reload();

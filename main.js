a7ede1e9d51fd99b298e655376704ed4464f87bb
woncodes <64626054+woncodes@users.noreply.github.com>
2024-05-13 20:53:20 -0400

    start from scratch

--- main.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector('body');
-const container = document.querySelector('#grid-container');
-
-const title = document.querySelector('#title-text');
-title.addEventListener('click', () => {
-  window.location.reload();
-})

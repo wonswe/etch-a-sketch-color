1473d34e5641e6d53fc2c8c147924d9875a4c8fa
woncodes <64626054+woncodes@users.noreply.github.com>
2024-06-11 15:53:17 -0400

    add getColor function to generate and return a random RGB color value

--- main.js
+++ main.js
@@ -62,3 +62,14 @@ function createGrid() {
   draw();
 }
 dimensionInput.addEventListener("input", createGrid);
+
+// Generate random RGB color // 
+function getColor() {
+  let symbols = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color = color + symbols[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
